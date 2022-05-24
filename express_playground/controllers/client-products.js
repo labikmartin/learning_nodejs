f43d4dee3f4b2e31e300bcc759bd481fa8ff8c49
Labik Martin <labikm@cngroup.dk>
2022-05-24 22:42:49 +0200

    dummy db && promisify product calls

--- express_playground/controllers/client-products.js
+++ express_playground/controllers/client-products.js
@@ -1,20 +1,25 @@
-import { STATUS } from '../constants/status-codes.js';
 import { Product } from '../models/product.js';
 import { routes } from '../routes/index.js';
+import { logError } from '../utils/logging.js';
 
 export function renderProducts(_, response) {
-  const productList = Product.fetchAll();
-  console.log({ productList });
-  response.render(routes.root.template, {
-    pageTitle: '📦 My products',
-    pageName: routes.root.name,
-    layout: './layouts/page.html',
-    productList
-  });
+  Product.fetchAll()
+    .then((productList) => {
+      console.log({ productList });
+
+      response.render(routes.root.template, {
+        pageTitle: '📦 My products',
+        pageName: routes.root.name,
+        layout: './layouts/page.html',
+        productList
+      });
+    })
+    .catch(logError);
 }
 
 export function deleteProduct(request, response, next) {
   const { id } = request.params;
-  Product.delete(id);
-  response.redirect(routes.root.path);
+  Product.delete(id)
+    .then(() => response.redirect(routes.root.path))
+    .catch(logError);
 }
---
+++ express_playground/utils/logging.js
@@ -0,0 +1,3 @@
+export function logError(data) {
+  console.error('\x1b[31m%s\x1b[0m', data);
+}
